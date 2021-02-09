abf8566efc14c7d1e563a438b6f750c8242ccaa6
furqyz <76882861+furqyz@users.noreply.github.com>
2021-02-10 01:06:56 +0300

    Update 8ball.js

--- komutlar/8ball.js
+++ komutlar/8ball.js
@@ -32,7 +32,7 @@ exports.run = function(client, message, args) {
 
     var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
 
-    if(!soru) return message.reply('Bir soru belirt. **Doğru Kullanım**: **r!**8ball soru')
+    if(!soru) return message.reply('Bir soru belirt. **Doğru Kullanım**: 8ball soru')
     else message.channel.send(cevap)
 
 };  
